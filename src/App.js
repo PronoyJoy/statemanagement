8191e8381a834c86d1f97e7162a0ae355f94a240
PronoyJoy <pronoy.das.developer@gmail.com>
2023-09-07 15:47:38 +0600

    app.js updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,49 @@
-import React, {  useReducer } from "react";
+import React, { useReducer } from "react";
 
+
+
+function List() {
+  const [state, dispatch] = useReducer(
+    (state, action) => {
+
+      return{ ...state,
+        ...action}
+   
+  }, {
+    first: "",
+    last: ""
+  })
+
+  return (
+    <div>
+
+      <input
+        type="text"
+        value={state.first}
+        onChange={(e) => {
+          const first = e.target.value;
+          dispatch({ first: first });
+        }}
+      />
+       <input
+        type="text"
+        value={state.last}
+        onChange={(e) => {
+          const last = e.target.value;
+          dispatch({ last: last});
+        }}
+      />
+      <div>First : {state.first} , last:{state.last}</div>
+
+    </div>
+
+  )
+
+
+
+}
 
 function App() {
-
-
 
   const [state, dispatch] = useReducer(
     (state, action) => {
@@ -37,7 +77,11 @@ function App() {
   return (
 
     <div className="App">
+       
 
+       <List/>
+
+       
 
       <div>
 
